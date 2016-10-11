77a93a7a2349285d124821f19d3db6ddff699128
Jean Joeris <joeris.peter@gmail.com>
2016-10-11 16:04:29 -0600

    Clean up gems, dependencies and authorization URL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010213535) do
+ActiveRecord::Schema.define(version: 20161011201038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20161010213535) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "refresh_token"
   end
 
 end

ab70de2a87115c762cfc56f43e2d81d47910a12f
Jean Joeris <joeris.peter@gmail.com>
2016-10-12 16:33:32 -0600

    Add post PORO

---
+++ app/models/post.rb
@@ -0,0 +1,24 @@
+class Post
+  def initialize(post_data)
+    @data = post_data
+  end
+
+  def title
+    data[:title]
+  end
+
+  def author
+    data[:author]
+  end
+
+  def url
+    data[:url]
+  end
+
+  def score
+    data[:score]
+  end
+
+  private
+      attr_reader :data
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe Post, type: :model do
-  it "has an author, url, subreddit and upvote score" do
-    post = Post.new({ data: { url: "https://foo.com", author: "test", score: 2 } })
+  it "has an title, author, url, subreddit and upvote score" do
+    post = Post.new({ title: "This is a test", url: "https://foo.com", author: "test", score: 2 })
 
+    expect(post.title).to eq("This is a test")
     expect(post.url).to eq("https://foo.com")
     expect(post.author).to eq("test")
     expect(post.score).to eq(2)
